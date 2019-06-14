9dc1173f3eed9f5dd0d2e8e66b502b94b68a4346
Damian Mooyman <damian.mooyman@littlegiant.co.nz>
2019-06-14 17:28:36 +1200

    Build base for item panel

--- forms.py
+++ forms.py
@@ -1,11 +1,16 @@
+from django.db.models.fields.reverse_related import ForeignObjectRel
 from django.template.loader import render_to_string
 
 from wagtail.admin.edit_handlers import EditHandler
 
 
 class CowTailPanel(EditHandler):
-    template = "cowtail/gridfield.html"
+    """
+    Gridfield panel for editing a relation on a parent object
+    """
+    template = 'cowtail/gridfield.html'
     label = None
+    db_field: ForeignObjectRel = None
 
     def __init__(self, relation_name, heading='', label='', *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -13,10 +18,19 @@ class CowTailPanel(EditHandler):
         self.heading = heading or label
         self.label = label
 
+    # List of field names this grid should display
+    # Excludes final "actions" column
+    def summary_fields(self):
+        # todo Get fields from this
+        print(self.instance)
+        print(self.db_field.related_model)
+        pass
+
     def clone_kwargs(self):
         kwargs = super().clone_kwargs()
         kwargs.update(
             relation_name=self.relation_name,
+            heading=self.heading,
             label=self.label,
         )
         return kwargs
@@ -25,8 +39,32 @@ class CowTailPanel(EditHandler):
         manager = getattr(self.model, self.relation_name)
         self.db_field = manager.rel
 
+    def bind_to(self, instance=None, **kwargs):
+        if instance is not None:
+            self.instance = instance
+        return super(CowTailPanel, self).bind_to(instance=instance, **kwargs)
+
     def render(self):
+        self.summary_fields()
+        # todo get items from self.instance
         return render_to_string(self.template, {
             'self': self,
-            'items': [] # todo wrap each item in an inline panel widget
+            'items': []  # todo wrap each item in an inline panel widget
+        })
+
+
+class CowTailItemPanel:
+    """
+    Row item representing a single item in a grid
+    """
+    template = 'cowtail/gridfield_item.html'
+
+    def __init__(self, panel, item):
+        self.panel = panel
+        self.item = item
+
+    def render(self):
+        # todo - get parent summary fields
+        return render_to_string(self.template, {
+            'self': self,
         })

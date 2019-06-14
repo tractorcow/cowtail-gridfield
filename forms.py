9761529cb3f56a61e04c8b67ffe683c8b8e676f1
Damian Mooyman <damian.mooyman@littlegiant.co.nz>
2019-06-14 16:44:40 +1200

    Add custom CSS Update templates for grid view Play around with ORM

--- forms.py
+++ forms.py
@@ -7,19 +7,26 @@ class CowTailPanel(EditHandler):
     template = "cowtail/gridfield.html"
     label = None
 
-    def __init__(self, relation_name, label='', *args, **kwargs):
+    def __init__(self, relation_name, heading='', label='', *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.relation_name = relation_name
+        self.heading = heading or label
         self.label = label
 
     def clone_kwargs(self):
         kwargs = super().clone_kwargs()
         kwargs.update(
             relation_name=self.relation_name,
+            label=self.label,
         )
         return kwargs
+
+    def on_model_bound(self):
+        manager = getattr(self.model, self.relation_name)
+        self.db_field = manager.rel
 
     def render(self):
         return render_to_string(self.template, {
             'self': self,
+            'items': [] # todo wrap each item in an inline panel widget
         })
--- wagtail_hooks.py
+++ wagtail_hooks.py
@@ -1,4 +1,6 @@
 from django.conf.urls import include, url
+from django.templatetags.static import static
+from django.utils.html import format_html
 from wagtail.core import hooks
 from wagtail.core.models import Page
 
@@ -10,3 +12,11 @@ def register_page_gridfield():
     return [
         url(r'^pages/(?P<recordid>\d+)/edit/', include(urls), kwargs={'model': Page}),
     ]
+
+
+@hooks.register('insert_editor_css')
+def register_gridfield_css():
+    return format_html(
+        '<link rel="stylesheet" href="{}" />',
+        static('cowtail/dist/cowtail.css')
+    )
